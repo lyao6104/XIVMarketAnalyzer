8482f3db110e98976a469268af64eb634bb5af50
Vithralia <yaol5785@gmail.com>
2022-06-09 22:43:43 -0400

    Remove src/xivapi_query.py since it's unused

--- src/xivapi_query.py
+++
@@ -1,6 +0,0 @@
-# Module defines a class for representing XIVAPI ElasticSearch Queries (https://xivapi.com/docs/Search#advanced)
-
-
-class Query(object):
-    def __init__(self) -> None:
-        self.indexes = ["Item"]
